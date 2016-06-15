13a808d4b7d50b03f1684a45c9f7019ae97c188b
muxico73 <muxico73@gmail.com>
2016-06-15 17:36:22 -0500

    still not working but the code is there. Breaking due to async calling.

--- wAdmin/wAdmin/gnas.js
+++ wAdmin/wAdmin/gnas.js
@@ -107,18 +107,81 @@ feedparser.on('readable', function() {
 
 });
 
+// we need MONGO and monk
+// DB code
+var mongo = require('mongodb');
+var monk = require('monk');
+//var db = monk(process.env.IP + ':27017/alertlist');
+var dbFIPS = monk('localhost:27017/FIPS');
+var dbCust = monk('localhost:27017/customer');
+var dbZip = monk('localhost:27017/zip');
+
+//TODO:
+// Michael, this was my attempt to sync the callbacks from the db.
+//NOT WORKING now
+//
+// This approach might work:
+// http://www.codexpedia.com/javascript/nodejs-sync-nested-mysql-queries/
+// I have to leave for today. we can catch up tomorrow
+// the queries below should all work, but they're breaking because they run async
+
+function countycallback (e,counties){
+  var zipcollection = dbZip.get('zipcollection');
+  zipcollection.find({county: counties[0].county, state: counties[0].state}, zipcb);
+}
+
+function zipcb(e,zips){
+  var custcollection = dbCust.get('customercollection');
+  custcollection.find({zipcode: zips[0].zipcode},custcb);
+}
+
+function custcb(e,cust){
+  custList.push(cust);
+}
+
+var discoverPhones = function (inputArray) {
+  var countyList = [];
+  var zipList = [];
+  var custList = [];
+  //from FIPS, get County
+  var collection = dbFIPS.get('countycollection');
+  if (inputArray.length < 0) return "";
+
+  for (i = 0; i < inputArray.length; i++) {
+    collection.find({FIPS: inputArray[i]}, countycallback);
+  }
+
+  // at this point, custList has everything
+  // need to return a list of phone numbers, with the right formatting, comma separated
+  return formatEmails(custList);
+}
+
+function formatEmails(custList) {
+  if (custList.length < 0) return "";
+  var emails = "";
+  for (i = 0; i < custList.length; i++) {
+    emails = emails + custList[i].telephonenumber.replace(/-/g, '') + "@txt.att.net, ";
+  }
+  //need to trim the last ', '
+  return emails.substring(0,emails.length-2);
+}
+
+
 feedparser.on('end', function() {
+  var customerPhones = '';
   var alertString = "";
   weatherEvents.forEach(function(value, key) {
     switch (value.severity) {
       case "Extreme":
         if ((value.certainty == "Observed" || value.certainty == "Likely")) {
+          customerPhones = discoverPhones(value.fipsArray);
           alertString += "Extreme weather event " + value.certainty + " - " + value.eventType + " - need to notify customers\r\n";
           alertString += value.fips + ": " + value.fipsArray + "\r\n";
         }
         break;
       case "Severe":
         if ((value.certainty == "Observed" || value.certainty == "Likely")) {
+          customerPhones = discoverPhones(value.fipsArray);
           alertString += "Severe weather event " + value.certainty + " - " + value.eventType + " - need to notify customers\r\n";
           alertString += value.fips + ": " + value.fipsArray + "\r\n";
         }
@@ -127,10 +190,11 @@ feedparser.on('end', function() {
 
   });
 
+  customerPhones = '2176491422@txt.att.net, 3097509512@txt.att.net, 2172026548@txt.att.net';
   console.log("entering mail");
   var mailOptions = {
-    from: '"GNAS Alert" <gnas@alvinbaker.com>', // sender address
-    to: '2176491422@txt.att.net, 3097509512@tt.att.net', // list of receivers
+    from: '"State Farm" <gnas@alvinbaker.com>', // sender address
+    to: customerPhones, // list of receivers
     subject: '', // Subject line
     text: "Severe weather alert - take cover now!!!!", // plaintext body
     // html: '<b>Hello world 🐴</b>' // html body
