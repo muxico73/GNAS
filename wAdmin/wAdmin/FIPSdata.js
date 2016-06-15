e7afe99788260036759f32073c132cd84680de17
muxico73 <muxico73@gmail.com>
2016-06-15 13:22:56 -0700

    FIPS works now

---
+++ wAdmin/wAdmin/FIPSdata.js
@@ -0,0 +1,47 @@
+//var geonames = require('geonames-stream'),
+var request = require('request'),
+    through = require('through2');
+    
+var mongo = require('mongodb');
+var monk = require('monk');
+//var db = monk(process.env.IP + ':27017/alertlist');
+var db = monk('localhost:27017/FIPS');
+
+function writeToDB (data) {
+  // Set a collection
+  var userCol = db.get('countycollection');
+    //define error function
+  var errFunc = function (err, doc) {
+      if (err) {
+          // If it failed, return error
+          console.log("There was a problem adding " + data.toJSON().name + " to the database. Error: " + err.message);
+      } else {
+         // console.log("Inserted record: " + element.name + " succesffully.");
+      }
+  };
+
+    userCol.insert(sanitize(data), errFunc);
+}
+
+function sanitize (line) {
+    text = line.toString();
+    var values = text.split(",");
+    var result = {
+        state: values[0],
+        FIPS: "0" + values[1] + values[2],
+        county: values[3]
+    };
+    return result;
+}
+
+var linereader = require('through2-linereader');
+
+var fs = require('fs');
+fs.createReadStream('national_county.txt', 'utf8')
+    .pipe(linereader())
+    .on('data', function(line){
+        writeToDB(line);
+})
+
+
+console.log("done!");
